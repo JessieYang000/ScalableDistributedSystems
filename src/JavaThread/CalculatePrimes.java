45dfed5b34ffd62e62c267397bbabb01bc639c67
JessieYang000 <yang.jieq@northeastern.edu>
2024-09-25 22:48:20 -0400

    add more comments

--- src/JavaThread/CalculatePrimes.java
+++ src/JavaThread/CalculatePrimes.java
@@ -2,11 +2,21 @@ package JavaThread;
 
 /**
  * JavaThread.CalculatePrimes -- calculate as many primes as we can in ten seconds
+ * Two threads in this example:
+ * Main Thread: Handles the setup, starts the CalculatePrimes thread, sleeps for 10 seconds, and then signals the
+ *              prime-calculating thread to stop.
+ * CalculatePrimes Thread: Runs the run() method, calculating prime numbers while the main thread sleeps, until it is
+ *                         stopped after 10 seconds.
  */
+//extends to Thread class means this class is a thread and can be executed concurrently with other threads
 public class CalculatePrimes extends Thread {
     public static final int MAX_PRIMES = 1000000;
     public static final int TEN_SECONDS = 10000;
+
+    //volatile will allow changes on this variable is synchronized among threads: main and CalculatePrimes
     public volatile boolean finished = false;
+
+    //when a thread is started, this method is invoked automatically in the new thread of execution
     public void run() {
         int[] primes = new int[MAX_PRIMES];
         int count = 0;
@@ -32,7 +42,7 @@ public class CalculatePrimes extends Thread {
     }
     public static void main(String[] args) {
         CalculatePrimes calculator = new CalculatePrimes();
-        calculator.start();
+        calculator.start(); //start a new thread
         try {
             Thread.sleep(TEN_SECONDS);
         } catch (InterruptedException e) {
